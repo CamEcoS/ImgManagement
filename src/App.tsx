f5d87a6cdcc337a320d52f9b944449b69db4a781
Camille Basbous <camille.basbous@eco-surv.co.uk>
2022-01-14 13:15:28 +0000

    ImGMngt 1.27

--- src/App.tsx
+++ src/App.tsx
@@ -28,15 +28,7 @@ export default function App() {
     closeModal: close
   })
 
-  const [sigProps, setSig] = useState<SigMain>({
-    contHeight: 80,
-    contWidth: 80,
-    acceptedFormat: "image/png, image/jpg, image/jpeg",
-    startingOptions: "Image",
-    currentState: null,
-    defaultFile: imgDef,
-    closeModal: close
-  })
+ 
 
 
 
@@ -58,8 +50,8 @@ export default function App() {
         </h4>
       </div>
 
-      {/* {modal ? <ImgSlots attribute={mainProps} /> : null}  */}
-      {modal ? <Signature attribute={sigProps} /> : null}
+      {modal ? <ImgSlots attribute={mainProps} /> : null} 
+ 
 
 
 
